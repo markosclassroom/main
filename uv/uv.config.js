34c06ffa5b0e490f628200ed4439025442c6eb49
Marko <marko.vityk@gmail.com>
2025-05-15 07:54:56 -0500

    quick adsense thing

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/',
-    bare: '/bare/',
+    bare: '/uv/bare/', // Automatically uses the current site as the base
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
